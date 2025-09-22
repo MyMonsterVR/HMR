dd540a6aef7c955899cb1b6cf81119a3d61bcac2
Rasmus Høllund Kristensen <31127327+MyMonsterVR@users.noreply.github.com>
2025-09-22 10:08:54 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import chalk from "chalk"
+import chokidar from 'chokidar'
+import WebSocket from 'ws'
+
+class Server {
+    constructor() {
+        console.log(chalk.red("Main"));
+    }
+    
+    // Load config
+    
+    // Load handlers
+    
+    // Watch for file changes
+    
+    // Open WS connection for client js
+    
+    // Stream the changes to the user
+}
+
+const server = new Server();
